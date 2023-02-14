1d5ad0663d6c3b2dd111eb4549a607f1310e0048
TechQuery <shiy2008@gmail.com>
2023-02-15 03:17:54 +0800

    [optimize] Session Box supports Manual mode [optimize] update Upstream packages

--- src/components/SessionBox.tsx
+++ src/components/SessionBox.tsx
@@ -2,27 +2,59 @@ import { Button, Dialog } from '@antmjs/vantui';
 import { Input } from '@tarojs/components';
 import { observable } from 'mobx';
 import { observer } from 'mobx-react';
-import { PropsWithChildren, PureComponent } from 'react';
+import { MouseEvent, PropsWithChildren, PureComponent } from 'react';
+import { sleep } from 'web-utility';
 
 import userStore from '../store/User';
 
-export type SessionBoxProps = PropsWithChildren<{}>;
+export type SessionBoxProps = PropsWithChildren<{
+  autoCover?: boolean;
+}>;
 
 @observer
 export class SessionBox extends PureComponent<SessionBoxProps> {
+  @observable
+  showDialog = false;
+
   @observable
   mobilePhone = '';
 
   @observable
   code = '';
 
-  sendSMS = () => userStore.sendSMSCode(this.mobilePhone);
+  @observable
+  interval = 0;
 
-  signIn = () => userStore.signInWithSMS(this.mobilePhone, this.code);
+  checkSession = (event: MouseEvent<HTMLElement>) => {
+    if (userStore.session) return;
+
+    event.stopPropagation();
+
+    this.showDialog = true;
+  };
+
+  sendSMS = async () => {
+    userStore.sendSMSCode(this.mobilePhone);
+
+    this.interval = 60;
+
+    do {
+      await sleep(1);
+    } while (--this.interval < 1);
+  };
+
+  signIn = async () => {
+    await userStore.signInWithSMS(this.mobilePhone, this.code);
+
+    this.interval = 0;
+    this.showDialog = false;
+  };
 
   renderDialog() {
+    const { mobilePhone, code, interval } = this;
+
     return (
-      <Dialog show>
+      <Dialog show showConfirmButton={false} closeOnClickOverlay={false}>
         <div className="p-3">
           <Input
             className="p-3"
@@ -41,13 +73,21 @@ export class SessionBox extends PureComponent<SessionBoxProps> {
               <Button
                 className="text-nowrap"
                 type="primary"
+                disabled={!mobilePhone || !!interval}
                 onClick={this.sendSMS}
               >
-                发送验证码
+                {interval ? `${interval}s` : '发送验证码'}
               </Button>
             </div>
           </div>
-          <Button type="primary" formType="submit" onClick={this.signIn}>
+          <Button
+            className="mt-3"
+            block
+            type="primary"
+            formType="submit"
+            disabled={!mobilePhone || !code}
+            onClick={this.signIn}
+          >
             登录
           </Button>
         </div>
@@ -56,14 +96,23 @@ export class SessionBox extends PureComponent<SessionBoxProps> {
   }
 
   render() {
-    const { children } = this.props;
+    const { autoCover, children } = this.props,
+      { showDialog } = this,
+      { session } = userStore;
 
     return (
-      <>
-        {children}
+      <div className="position-relative">
+        {(!autoCover || session) && children}
 
-        {this.renderDialog()}
-      </>
+        {!autoCover && !session && !showDialog && (
+          <div
+            className="position-absolute left-0 top-0 w-100 h-100"
+            style={{ zIndex: 1000 }}
+            onClick={this.checkSession}
+          />
+        )}
+        {(autoCover ? !session : showDialog) && this.renderDialog()}
+      </div>
     );
   }
 }
